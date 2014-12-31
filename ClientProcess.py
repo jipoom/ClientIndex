8b82c9169c4b5862a4150106fa83509639d4cf6e
Jipoom <jipoom@gmail.com>
2014-12-31 23:00:35 +0700

    multiprocessing client

--- ClientProcess.py
+++ ClientProcess.py
@@ -1,4 +1,5 @@
-import socket, datetime, time, threading
+import socket, datetime, time, threading, sys
+from multiprocessing import Process
 
 KEEPALIVE_TIME_GAP = 2; #seconds
 HOST = '127.0.0.1'   # Symbolic name meaning all available interfaces
@@ -9,9 +10,11 @@ now=datetime.datetime.now()
 def getExecuteTime():
     now=datetime.datetime.now()
     return time.mktime(now.timetuple()) 
+              
 
 class keepAliveThread (threading.Thread):
     def __init__(self,keepAliveTime,nextkeepAliveTime):
+        self.process = None
         threading.Thread.__init__(self)
         self.keepAliveTime = keepAliveTime
         self.nextkeepAliveTime = nextkeepAliveTime
@@ -30,13 +33,29 @@ class keepAliveThread (threading.Thread):
                     self.nextkeepAliveTime = self.keepAliveTime+KEEPALIVE_TIME_GAP
                     print "SendHeartBeat"
                     client.send ('HeartBeat')
+                   
                 except socket.error:
                     #came out of loop
                     print "Master is down!!!"
                     client.close()
                     break
-keepAliveTime = getExecuteTime()
-nextkeepAliveTime = keepAliveTime+KEEPALIVE_TIME_GAP
-keepAliveThread = keepAliveThread(keepAliveTime,nextkeepAliveTime)
-# Start new Threads
-keepAliveThread.start()
+            
+def doIndexing():
+    keepAliveTime = getExecuteTime()
+    nextkeepAliveTime = keepAliveTime+KEEPALIVE_TIME_GAP
+    HeartBeatThread = keepAliveThread(keepAliveTime,nextkeepAliveTime)
+    HeartBeatThread.start()
+    while True:
+        keepAliveTime =  getExecuteTime()
+        if keepAliveTime >= nextkeepAliveTime:
+            nextkeepAliveTime = keepAliveTime+KEEPALIVE_TIME_GAP
+            print "test"
+            sys.stdout.flush()
+        
+
+
+if __name__ == '__main__':
+    # Listen for master
+    p = Process(target=doIndexing, args=())
+    p.start()
+    p.join()
